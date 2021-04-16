e7f66c3f802f1b6d03fec5045cb5c483668627a6
mratanpara <mohitratanpara2001@gmail.com>
2021-04-16 12:08:09 +0530

    Tutorial 8 AUI done finally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddvideoComponent } from './addvideo/addvideo.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'videos',component:VideosComponent,canActivate:[AuthGuard]},
+  {path:'addvideo',component:AddvideoComponent,canActivate:[AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
